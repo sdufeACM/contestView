5324cc4faa75e270b92c31717c3605ad7d5a5e53
xia0ne <1223605525@qq.com>
2023-06-05 16:29:15 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { Table,Typography } from 'antd';
 import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import {FieldTimeOutlined } from '@ant-design/icons';
+const { Text } = Typography;
 
+const { Column } = Table;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true); // 加载状态
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const now = new Date().getTime();
+        axios.get('https://contests.sdutacm.cn/contests.json')
+            .then(res => {
+                setLoading(false);
+                setData(res.data
+                    .filter(item => new Date(item.start_time).getTime() > now) // 筛选未开始的比赛
+                    .map(item => {
+                        // 将 UTC+0 时间转换为北京时间
+                        const startTime = new Date(item.start_time).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' });
+                        const endTime = new Date(item.end_time).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' });
+                        item.start_time = startTime.replace(/\//g, '-').replace(/\b\d\b/g, '0$&'); // 转换格式
+                        item.end_time = endTime.replace(/\//g, '-').replace(/\b\d\b/g, '0$&');
+                        return item;
+                    }));
+            })
+            .catch(err => console.log(err));
+    }, []);
+    const getTime = (record) => {
+        //获取倒计时 如果是大于1天的话，就显示天数，否则显示小时，如果不到1小时，就显示分钟
+        const now = new Date().getTime();
+        const startTime = new Date(record.start_time).getTime();
+        if (startTime > now) {
+            const time = (startTime - now) / 1000;
+            if (time > 86400) {
+                return Math.floor(time / 86400) + "天";
+            } else if (time > 3600) {
+                return Math.floor(time / 3600) + "小时";
+            } else {
+                return Math.floor(time / 60) + "分钟";
+            }
+
+        }
+    }
+    const renderInfo = (text, record) => {
+        return (
+            <a href={record.link} target="_blank" rel="noreferrer">
+                {text}   <Text keyboard><FieldTimeOutlined />{getTime(record)}</Text>
+            </a>
+        )
+    }
+
+    return (
+        <div>
+            <div id="div1">
+                <fieldset>
+                    <h2>最近的比赛</h2>
+                    <Table dataSource={data} pagination={false} rowKey="id" loading={loading}>
+                        <Column title="来源" dataIndex="source" key="source" />
+                        <Column title="比赛名称" dataIndex="name" key="name" render={renderInfo} />
+                        <Column title="开始时间" dataIndex="start_time" key="start_time" />
+                        <Column title="结束时间" dataIndex="end_time" key="end_time" />
+                    </Table>
+                </fieldset>
+            </div>
+            <div id="footer">
+                <p>Shandong University of Finance and Economics ACM Society</p>
+                <p>Copyright © 2023-2023 <a href="https://github.com/sdufeACM"> SDUFEACM开发组</a>. All Rights Reserved</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {FloatButton, notification} from "antd";
+import { QuestionCircleOutlined } from '@ant-design/icons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+      <>
+          <FloatButton
+              icon={<QuestionCircleOutlined />}
+              type="default"
+              style={{
+                  right: 94,
+              }}
+              tooltip={<div>关于</div>}
+              onClick={()=>{
+                  notification.open({
+                      message: '关于',
+                      description:
+                          <>
+                              sdufeACM比赛日历<br />
+                              react随便糊的，接口用的sdut的接口<br />
+                              <a href="https://github.com/xia0ne">YuRuiH</a>
+
+                          </>
+              })}}
+          />
+      </>
   </React.StrictMode>
 );
 
